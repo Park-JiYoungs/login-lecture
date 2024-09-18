6ed4d2cc0a642fc41754ef4a2f2618890ed003f6
Park JiYoungs <piy0305@gmail.com>
2024-09-18 20:42:05 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,8 +1,10 @@
 "use script";
 
 const app = require("../app");
+const logger = require("../src/config/logger.js");
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log("서버 가동");
+    logger.info(`${port} 포트에서 서바가 가동되었습니다.`);
 });
