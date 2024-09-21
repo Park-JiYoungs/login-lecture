1ed09e11b38c397db9381402f77c6e1de5f861fe
Park JiYoungs <piy0305@gmail.com>
2024-09-21 16:31:09 +0900

    회원가입시 아이디생성규칙 추가 및 중복체크 개선

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -6,8 +6,7 @@ class UserStorage {
 
     static getUserInfo(id) {
         return new Promise((resolve, reject) => {
-
-            const query = "up_selectUserInfo";
+            const sql = "up_selectUserInfo";
             db.request()
                 .input("id", id)
                 .execute(sql, (err, data) => {
@@ -19,12 +18,12 @@ class UserStorage {
 
     static async save(userInfo) {
         return new Promise((resolve, reject) => {
-            const query = "up_insertUserInfo";
-            const sql = db.request()
+            const sql = "up_insertUserInfo";
+            db.request()
                 .input("id", userInfo.id)
                 .input("name", userInfo.name)
                 .input("psword", userInfo.psword)
-                .execute(query, (err, data) => {
+                .execute(sql, (err, data) => {
                     if (err) reject (`${err}`);
                     else {
                         data = data.recordset[0];
@@ -36,10 +35,10 @@ class UserStorage {
 
     static async checkId(id) {
         return new Promise((resolve, reject) => {
-            const sql = "select id from users where id = @id";
+            const sql = "up_selectUserInfo";
             db.request()
                 .input("id", id)
-                .query(sql, (err, data) => {
+                .execute(sql, (err, data) => {
                     if (err) reject (`${err}`);
                     else resolve(data.recordset[0]);
                 })
